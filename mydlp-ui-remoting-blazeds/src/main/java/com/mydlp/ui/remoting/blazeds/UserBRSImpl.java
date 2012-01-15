43407957e84f0c9941390d92b8c79fc28e606313
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-01-15 20:46:04 +0200

    Added session toolbar.

--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserBRSImpl.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserBRSImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.flex.remoting.RemotingDestination;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.mydlp.ui.dao.UserDAO;
@@ -24,6 +25,15 @@ public class UserBRSImpl implements UserService
 	@Override
 	public void remove(AuthUser item) {
 		userDAO.remove(item);
+	}
+
+	@Override
+	public AuthUser getCurrentUser() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		AuthUser authUser = null;
+		if (principal instanceof AuthUser)
+			authUser = (AuthUser) principal;
+		return authUser;
 	}
 	
 	
--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserService.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserService.java
@@ -14,4 +14,6 @@ public interface UserService {
 	
 	public void remove(AuthUser item);
 	
+	public AuthUser getCurrentUser();
+	
 }
