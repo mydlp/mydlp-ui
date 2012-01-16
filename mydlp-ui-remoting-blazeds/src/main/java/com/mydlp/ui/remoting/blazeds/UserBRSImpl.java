9a2e6f6202fcc57077fd0aae647788c8e26da7c6
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-01-16 20:26:21 +0200

    Added session toolbar. Several refinements on main application

--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserBRSImpl.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/UserBRSImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.mydlp.ui.dao.UserDAO;
@@ -29,10 +30,14 @@ public class UserBRSImpl implements UserService
 
 	@Override
 	public AuthUser getCurrentUser() {
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		AuthUser authUser = null;
-		if (principal instanceof AuthUser)
-			authUser = (AuthUser) principal;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		UserDetails userDetails = null;
+		if (principal instanceof UserDetails) {
+			userDetails = (UserDetails) principal;
+			String username = userDetails.getUsername();
+			authUser = userDAO.findByName(username);
+		}
 		return authUser;
 	}
 	
