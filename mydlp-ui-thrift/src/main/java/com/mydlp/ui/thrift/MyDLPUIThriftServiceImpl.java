d028a36e2496f08dafaffe8b192aadcecd865772
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-01-25 00:17:34 +0200

    Tiny fix.

--- mydlp-ui-thrift/src/main/java/com/mydlp/ui/thrift/MyDLPUIThriftServiceImpl.java
+++ mydlp-ui-thrift/src/main/java/com/mydlp/ui/thrift/MyDLPUIThriftServiceImpl.java
@@ -29,7 +29,7 @@ public class MyDLPUIThriftServiceImpl implements MyDLPUIThriftService {
 			destroy();
 		}
 		try {
-			transport = new TSocket("localhost", 9090);
+			transport = new TSocket("localhost", 9092);
 			transport.open();
 
 			TProtocol protocol = new TBinaryProtocol(transport);
