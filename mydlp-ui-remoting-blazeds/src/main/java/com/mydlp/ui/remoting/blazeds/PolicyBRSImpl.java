34cd83d1c13bae5acd8e5efd34d564f692bc32a6
Ozgen MUZAC <ozgen@medrateknoloji.com>
2012-01-25 12:13:28 +0200

    Tiny refinements.

--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/PolicyBRSImpl.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/PolicyBRSImpl.java
@@ -1,28 +1,24 @@
 package com.mydlp.ui.remoting.blazeds;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.stereotype.Service;
 
-//import com.mydlp.ui.thrift.MyDLPUIThriftService;
+import com.mydlp.ui.thrift.MyDLPUIThriftService;
 
 @Service("policyBRS")
 @RemotingDestination
 public class PolicyBRSImpl implements PolicyService
 {
+	
+	@Autowired
+	@Qualifier("thriftService")
+	protected MyDLPUIThriftService thriftService;
 
 	@Override
 	public void compilePolicy(Integer policyId) {
-		// TODO Auto-generated method stub
-		
+		thriftService.compileFilter(policyId);
 	}
-	
-//	@Autowired
-//	@Qualifier("thriftService")
-	//protected MyDLPUIThriftService thriftService;
-
-//	@Override
-	//public void compilePolicy(Integer policyId) {
-	//	thriftService.compileFilter(policyId);
-	//}
 
 }
