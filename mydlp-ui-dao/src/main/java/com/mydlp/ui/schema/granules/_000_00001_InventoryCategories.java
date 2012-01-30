f4ac983c54ff83b0332a28dfedd7b10774290182
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-01-30 09:10:33 +0200

    Added editable property and iseditenabled isaddenabled methods

--- mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_000_00001_InventoryCategories.java
+++ mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_000_00001_InventoryCategories.java
@@ -7,23 +7,39 @@ public class _000_00001_InventoryCategories extends AbstractGranule {
 
 	@Override
 	protected void callback() {
+		InventoryCategory predefined = new InventoryCategory();
+		predefined.setNameKey("inventory.predefined");
+		predefined.setEditable(false);
+		getHibernateTemplate().save(predefined);
+		
 		InventoryCategory networks = new InventoryCategory();
 		networks.setNameKey("inventory.networks");
+		networks.setEditable(false);
+		networks.setCategory(predefined);
 		getHibernateTemplate().save(networks);
 		
 		InventoryCategory informationTypes = new InventoryCategory();
-		informationTypes.setNameKey("inventory.informationTypes");
-		InventoryCategory predefinedInformationTypes = new InventoryCategory();
-		predefinedInformationTypes.setNameKey("inventory.informationTypes.predefined");
-		predefinedInformationTypes.setCategory(informationTypes);
-		
+		informationTypes.setNameKey("inventory.informationTypes.predefined");
+		informationTypes.setEditable(false);
+		informationTypes.setCategory(predefined);
 		getHibernateTemplate().saveOrUpdate(informationTypes);
-		getHibernateTemplate().saveOrUpdate(predefinedInformationTypes);
-		
 		
 		InventoryCategory users = new InventoryCategory();
 		users.setNameKey("inventory.users");
+		users.setEditable(false);
+		users.setCategory(predefined);
 		getHibernateTemplate().save(users);
+		
+		InventoryCategory destinations = new InventoryCategory();
+		destinations.setNameKey("inventory.destinations");
+		destinations.setEditable(false);
+		destinations.setCategory(predefined);
+		getHibernateTemplate().save(destinations);
+		
+		InventoryCategory userDefined = new InventoryCategory();
+		userDefined.setNameKey("inventory.userDefined");
+		userDefined.setEditable(false);
+		getHibernateTemplate().save(userDefined);
 	}
 
 }
--- mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/InventoryCategory.java
+++ mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/InventoryCategory.java
@@ -3,6 +3,7 @@ package com.mydlp.ui.domain;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -15,6 +16,7 @@ public class InventoryCategory extends InventoryBase {
 	private static final long serialVersionUID = -7173991108220189552L;
 	
 	protected List<InventoryBase> children;
+	protected Boolean editable;
 	
 	@OneToMany(mappedBy="category", cascade={CascadeType.ALL})
 	public List<InventoryBase> getChildren() {
@@ -23,6 +25,14 @@ public class InventoryCategory extends InventoryBase {
 	
 	public void setChildren(List<InventoryBase> children) {
 		this.children = children;
+	}
+	
+	@Column(nullable=false)
+	public Boolean getEditable() {
+		return editable;
+	}
+	public void setEditable(Boolean editable) {
+		this.editable = editable;
 	}
 
 }
