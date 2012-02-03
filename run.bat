50d5bc7419caf003147f6e304daba44961d01390
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-02-03 17:23:33 +0200

    Several refinements. Added Generic details container. Added inventoryitem tooltip.

--- run.bat
+++ run.bat
@@ -1,3 +1,2 @@
-cmd /c "mvn install"
 cmd /c "cd mydlp-ui-webapp\hsqldb && del /q MyDLP.script MyDLPLog.script && copy MyDLP.sample.log MyDLP.log && copy MyDLPLog.sample.log MyDLPLog.log"
-cmd /c "cd mydlp-ui-webapp && mvn jetty:run-war"
+cmd /c "mvn install && cd mydlp-ui-webapp && mvn jetty:run-war"
