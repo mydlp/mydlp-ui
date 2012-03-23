475af08bc626057aedc55512395f66deb66baf1e
Ozgen Muzac <ozgen@medrateknoloji.com>
2012-03-23 16:02:53 +0200

    Discovery Rule is added to ui.

---
+++ mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/DiscoveryRule.java
@@ -0,0 +1,14 @@
+package com.mydlp.ui.domain;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class DiscoveryRule extends Rule{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8283441409533257606L;
+
+}
