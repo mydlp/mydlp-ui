275f0e71600c146ddf2d6462ff32d50b9a77dcbc
H. Kerem Cevahir <hkerem@gmail.com>
2012-03-23 13:10:35 -0700

    Added alive pdf to unix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,5 @@ mvn install:install-file -DgroupId=dpHibernate -DartifactId=dpHibernate-core -Dp
 mvn install:install-file -DgroupId=dpHibernate -DartifactId=dpHibernate-springExtensions3.0 -Dpackaging=jar -Dversion=2.0-RC6 -Dfile=external-lib/dpHibernate-springExtensions3.0-2.0-RC6.jar -DgeneratePom=true
 
 mvn install:install-file -DgroupId=dpHibernate -DartifactId=dpHibernate -Dpackaging=swc -Dversion=2.0-RC6 -Dfile=external-lib/dpHibernate-2.0-RC6.swc -DgeneratePom=true
+
+mvn install:install-file -DgroupId=AlivePDF -DartifactId=AlivePDF -Dpackaging=swc -Dversion=0.1.5-RC -Dfile=external-lib/AlivePDF-0.1.5-RC.swc -DgeneratePom=true
