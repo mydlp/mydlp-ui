8d1ad4ac27eaa2848c0a8b06e2f3bd02b6d9abde
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-03-26 17:37:11 +0300

    Added discovery options

--- mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_000_00029_Config_End_Point.java
+++ mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_000_00029_Config_End_Point.java
@@ -27,13 +27,32 @@ public class _000_00029_Config_End_Point extends AbstractGranule {
 		Config syncInterval = new Config();
 		syncInterval.setKey("sync_interval");
 		syncInterval.setValue("300000");
+		
+		Config discoverFSInterval = new Config();
+		discoverFSInterval.setKey("discover_fs_interval");
+		discoverFSInterval.setValue("7200000");
+		
+		Config discoverFSPaths = new Config();
+		discoverFSPaths.setKey("discover_fs_paths");
+		discoverFSPaths.setValue("C:/Users;C:/Documents and Settings");
+		
+		Config discoverFSOnStartup = new Config();
+		discoverFSOnStartup.setKey("discover_fs_on_startup");
+		discoverFSOnStartup.setValue("false");
+		
+		Config ignoreDiscoverMaxSizeExceeded = new Config();
+		ignoreDiscoverMaxSizeExceeded.setKey("ignore_discover_max_size_exceeded");
+		ignoreDiscoverMaxSizeExceeded.setValue("true");
 
 		getHibernateTemplate().saveOrUpdate(usbSerialAccessControl);
 		getHibernateTemplate().saveOrUpdate(printMonitor);	
 		getHibernateTemplate().saveOrUpdate(logLevel);
 		getHibernateTemplate().saveOrUpdate(logLimit);	
 		getHibernateTemplate().saveOrUpdate(syncInterval);
-
+		getHibernateTemplate().saveOrUpdate(discoverFSInterval);
+		getHibernateTemplate().saveOrUpdate(discoverFSPaths);
+		getHibernateTemplate().saveOrUpdate(discoverFSOnStartup);
+		getHibernateTemplate().saveOrUpdate(ignoreDiscoverMaxSizeExceeded);
 	}
 
 }
