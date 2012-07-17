70f0f58e0723940285d35cac6788d2c4929023fc
Ozgen Muzac <ozgen@medrateknoloji.com>
2012-07-17 11:23:41 +0300

    Threshold field is removed for inappropriate indormation features.

--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/PolicyService.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/PolicyService.java
@@ -4,7 +4,7 @@ import org.springframework.security.access.annotation.Secured;
 
 import com.mydlp.ui.domain.AuthSecurityRole;
 
-@Secured(AuthSecurityRole.ROLE_ADMIN)
+@Secured({AuthSecurityRole.ROLE_ADMIN, AuthSecurityRole.ROLE_CLASSIFIER})
 public interface PolicyService {
 
 	public void compilePolicy(Integer policyId);
