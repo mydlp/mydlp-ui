6208fcc48563724c9c2d7d961b9b9940c72aed9d
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-08-01 12:46:58 +0300

    Several refinements NIO

--- mydlp-ui-webapp/src/main/java/com/mydlp/ui/framework/util/NIOUtil.java
+++ mydlp-ui-webapp/src/main/java/com/mydlp/ui/framework/util/NIOUtil.java
@@ -1,6 +1,5 @@
 package com.mydlp.ui.framework.util;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -10,9 +9,7 @@ import org.apache.commons.io.IOUtils;
 public class NIOUtil {
 	
 	public static ByteBuffer getWholeData(InputStream is) throws IOException {
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		IOUtils.copy(is, os);
-		return ByteBuffer.wrap(os.toByteArray());
+		return ByteBuffer.wrap(IOUtils.toByteArray(is));
 	}
 
 }
