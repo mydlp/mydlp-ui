d854e506a471b07c0b3087fce6c919f7466d96b9
Ozgen Muzac <ozgen@medrateknoloji.com>
2012-10-16 11:20:32 +0300

    Added Destination Object.

---
+++ mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/Destination.java
@@ -0,0 +1,25 @@
+package com.mydlp.ui.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Destination extends Item {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3817078775013482909L;
+
+	protected String destinationString;
+
+	@Column(nullable=false)
+	public String getDestinationString() {
+		return destinationString;
+	}
+
+	public void setDestinationString(String destinationString) {
+		this.destinationString = destinationString;
+	}
+
+}
