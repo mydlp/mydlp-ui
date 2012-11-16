a547d19d454d1e1314db5b58b3249fbf65f4d7dd
Ozgen Muzac <ozgen@medrateknoloji.com>
2012-11-16 14:27:54 +0200

    Tiny refienemet.

--- mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_001_00100_Config_Notification.java
+++ mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_001_00100_Config_Notification.java
@@ -10,7 +10,7 @@ public class _001_00100_Config_Notification extends AbstractGranule {
 
 		Config emailNotificationPage = new Config();	
 		emailNotificationPage.setKey("email_notification_message");
-		emailNotificationPage.setValue("hede hodo");
+		emailNotificationPage.setValue("Hello,\r\nThis is an auto-generated message. This message aims to inform you about some incidents that have been recently occurred and logged in your MyDLP system.\r\nYou are recieving this message because you have subscribed to be notified for incidents related to a rule in MyDLP.\r\nFor details, please log on to MyDLP Management Console and go to Logs screen.\r\nIf you do not want to recieve these emails, please contact to your system administrator.\r\nPlease do not reply this email.");
 
 		getHibernateTemplate().saveOrUpdate(emailNotificationPage);	
 
