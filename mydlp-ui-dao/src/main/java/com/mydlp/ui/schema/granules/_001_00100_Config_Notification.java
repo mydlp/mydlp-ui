49bad7bd00c8704b5b8b91b0757cca2b69eb8aa6
Ozgen Muzac <ozgen@medrateknoloji.com>
2012-11-23 15:54:41 +0200

    Tiny Refinement.

--- mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_001_00100_Config_Notification.java
+++ mydlp-ui-dao/src/main/java/com/mydlp/ui/schema/granules/_001_00100_Config_Notification.java
@@ -14,7 +14,7 @@ public class _001_00100_Config_Notification extends AbstractGranule {
 
 		Config emailNotificationBody = new Config();	
 		emailNotificationBody.setKey("email_notification_message");
-		emailNotificationBody.setValue("Hello,\r\nThis is an auto-generated message. This message aims to inform you about some incidents that have been recently occurred and logged in your MyDLP system.\r\nYou are recieving this message because you have subscribed to be notified for incidents related to a rule in MyDLP.\r\nFor details, please log on to MyDLP Management Console and go to Logs screen.\r\nIf you do not want to recieve these emails, please contact to your system administrator.\r\nPlease do not reply this email.");
+		emailNotificationBody.setValue("Hello,\nThis is an auto-generated message. This message aims to inform you about some incidents that have been recently occurred and logged in your MyDLP system.\nYou are recieving this message because you have subscribed to be notified for incidents related to a rule in MyDLP.\nFor details, please log on to MyDLP Management Console and go to Logs screen.\nIf you do not want to recieve these emails, please contact to your system administrator.\nPlease do not reply this email.");
 
 		getHibernateTemplate().saveOrUpdate(emailNotificationSubject);
 		getHibernateTemplate().saveOrUpdate(emailNotificationBody);	
@@ -22,3 +22,4 @@ public class _001_00100_Config_Notification extends AbstractGranule {
 	}
 
 }
+	
