a94cf2bc135ba485b5fad2b34ae93b53552501c8
Hseyin Kerem Cevahir <hkerem@gmail.com>
2012-12-03 00:33:48 +0200

    Added commons upload to the project/

--- mydlp-ui-webapp/src/main/java/com/mydlp/ui/framework/servlet/ToolsUploaderServlet.java
+++ mydlp-ui-webapp/src/main/java/com/mydlp/ui/framework/servlet/ToolsUploaderServlet.java
@@ -1,12 +1,18 @@
 package com.mydlp.ui.framework.servlet;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,30 +28,66 @@ public class ToolsUploaderServlet implements HttpRequestHandler {
 	private static final String ERROR = "error";
 	private static final String SUCCESS = "ok";
 	
-	protected static final int MAX_CONTENT_LENGTH = 10*1024*1024;
+	protected static final int MAX_MEMORY_SIZE = 1*1024*1024;
 
-	// testtoken : p127.0.0.1:8080-testtesttesttesttesttesttesttest
+	// testtoken : plocalhost:8080-testtesttesttesttesttesttesttest
 	
 	@Override
 	public void handleRequest(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String returnStr = ERROR;
+		
 		try {
-			if (req.getContentLength() < MAX_CONTENT_LENGTH)
+			boolean isMultipart = ServletFileUpload.isMultipartContent(req);
+			if (isMultipart)
 			{
-				byte[] readData = IOUtils.toByteArray(req.getInputStream());
-				System.out.println(new String(readData));
+				String tokenKey = null;
+				String fileName = null;
+				String filePath = null;
+				
+				DiskFileItemFactory factory = new DiskFileItemFactory();
+				factory.setSizeThreshold(MAX_MEMORY_SIZE);
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				List<FileItem> items = upload.parseRequest(req);
+				for (FileItem fileItem : items) {
+					if (fileItem.isFormField())
+					{
+						String fieldName = fileItem.getFieldName();
+						String fieldValue = fileItem.getString();
+						
+						if (fieldName.equals("tokenKey"))
+						{
+							tokenKey = fieldValue;
+						}
+						else if (fieldName.equals("fileName"))
+						{
+							fileName = fieldValue;
+						}
+					}
+					else
+					{
+						File tempFile = File.createTempFile("tools-uploader-data-", ".tmp");
+						fileItem.write(tempFile);
+						filePath = tempFile.getCanonicalPath();
+					}
+				}
+				
+				System.out.println(tokenKey + " " + fileName + " " + filePath);
+				
 				returnStr = SUCCESS;
 			}
 			else
 			{
-				logger.error("Content-Length is bigger than 10MB ; " + req.getContentLength());
+				logger.error("Reques is not multipart");
 			}
 		} catch (IOException e) {
 			logger.error("IOError occurred", e);
-		} catch (RuntimeException e) {
-			logger.error("Runtime error occured", e);
-		}
+		} catch (FileUploadException e) {
+			logger.error("FileUpload error occured", e);
+		} catch (Throwable e) {
+			logger.error("Unexpected error occured", e);
+		} 
+		
 		if (returnStr == null || returnStr.length() == 0)
 		{
 			logger.error("Returned empty or null value from thrift call");
