9749e4653a3b167d42cb3ffa46adbdd46e31d201
Ozgen Muzac <ozgen@mydlp.com>
2013-04-22 18:06:16 +0300

    Revert

--- mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/DocumentDatabaseRemoteStorage.java
+++ mydlp-ui-domain/src/main/java/com/mydlp/ui/domain/DocumentDatabaseRemoteStorage.java
@@ -2,13 +2,11 @@ package com.mydlp.ui.domain;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -34,7 +32,6 @@ public class DocumentDatabaseRemoteStorage extends AbstractEntity{
 	@ElementCollection
 	@CollectionTable(name="DocumentDatabaseExcludeFile", joinColumns=@JoinColumn(name="documentDatabaseRemoteStorage_id"))
 	@Column(name="excludeFilename")
-	@OneToMany(cascade={CascadeType.ALL})
 	public List<String> getExcludeList() {
 		return excludeList;
 	}
