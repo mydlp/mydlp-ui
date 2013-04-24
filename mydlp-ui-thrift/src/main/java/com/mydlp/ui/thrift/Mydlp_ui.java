d659f382c97c2db6840064f68adaca1f02eb078d
ozgenmuzac <ozgen@mydlp.com>
2013-04-24 03:03:38 -0700

    New thrift call is added.

--- mydlp-ui-thrift/src/main/java/com/mydlp/ui/thrift/Mydlp_ui.java
+++ mydlp-ui-thrift/src/main/java/com/mydlp/ui/thrift/Mydlp_ui.java
@@ -56,6 +56,8 @@ public class Mydlp_ui {
 
     public List<String> getRemoteStorageDir(int RSId) throws org.apache.thrift.TException;
 
+    public void startFingerprinting(int DDId) throws org.apache.thrift.TException;
+
   }
 
   public interface AsyncIface {
@@ -91,6 +93,8 @@ public class Mydlp_ui {
     public void pauseDiscoveryOnDemand(int RuleId, org.apache.thrift.async.AsyncMethodCallback<AsyncClient.pauseDiscoveryOnDemand_call> resultHandler) throws org.apache.thrift.TException;
 
     public void getRemoteStorageDir(int RSId, org.apache.thrift.async.AsyncMethodCallback<AsyncClient.getRemoteStorageDir_call> resultHandler) throws org.apache.thrift.TException;
+
+    public void startFingerprinting(int DDId, org.apache.thrift.async.AsyncMethodCallback<AsyncClient.startFingerprinting_call> resultHandler) throws org.apache.thrift.TException;
 
   }
 
@@ -441,6 +445,18 @@ public class Mydlp_ui {
         return result.success;
       }
       throw new org.apache.thrift.TApplicationException(org.apache.thrift.TApplicationException.MISSING_RESULT, "getRemoteStorageDir failed: unknown result");
+    }
+
+    public void startFingerprinting(int DDId) throws org.apache.thrift.TException
+    {
+      send_startFingerprinting(DDId);
+    }
+
+    public void send_startFingerprinting(int DDId) throws org.apache.thrift.TException
+    {
+      startFingerprinting_args args = new startFingerprinting_args();
+      args.setDDId(DDId);
+      sendBase("startFingerprinting", args);
     }
 
   }
@@ -1011,6 +1027,37 @@ public class Mydlp_ui {
       }
     }
 
+    public void startFingerprinting(int DDId, org.apache.thrift.async.AsyncMethodCallback<startFingerprinting_call> resultHandler) throws org.apache.thrift.TException {
+      checkReady();
+      startFingerprinting_call method_call = new startFingerprinting_call(DDId, resultHandler, this, ___protocolFactory, ___transport);
+      this.___currentMethod = method_call;
+      ___manager.call(method_call);
+    }
+
+    public static class startFingerprinting_call extends org.apache.thrift.async.TAsyncMethodCall {
+      private int DDId;
+      public startFingerprinting_call(int DDId, org.apache.thrift.async.AsyncMethodCallback<startFingerprinting_call> resultHandler, org.apache.thrift.async.TAsyncClient client, org.apache.thrift.protocol.TProtocolFactory protocolFactory, org.apache.thrift.transport.TNonblockingTransport transport) throws org.apache.thrift.TException {
+        super(client, protocolFactory, transport, resultHandler, true);
+        this.DDId = DDId;
+      }
+
+      public void write_args(org.apache.thrift.protocol.TProtocol prot) throws org.apache.thrift.TException {
+        prot.writeMessageBegin(new org.apache.thrift.protocol.TMessage("startFingerprinting", org.apache.thrift.protocol.TMessageType.CALL, 0));
+        startFingerprinting_args args = new startFingerprinting_args();
+        args.setDDId(DDId);
+        args.write(prot);
+        prot.writeMessageEnd();
+      }
+
+      public void getResult() throws org.apache.thrift.TException {
+        if (getState() != org.apache.thrift.async.TAsyncMethodCall.State.RESPONSE_READ) {
+          throw new IllegalStateException("Method call not finished!");
+        }
+        org.apache.thrift.transport.TMemoryInputTransport memoryTransport = new org.apache.thrift.transport.TMemoryInputTransport(getFrameBuffer().array());
+        org.apache.thrift.protocol.TProtocol prot = client.getProtocolFactory().getProtocol(memoryTransport);
+      }
+    }
+
   }
 
   public static class Processor<I extends Iface> extends org.apache.thrift.TBaseProcessor implements org.apache.thrift.TProcessor {
@@ -1040,6 +1087,7 @@ public class Mydlp_ui {
       processMap.put("stopDiscoveryOnDemand", new stopDiscoveryOnDemand());
       processMap.put("pauseDiscoveryOnDemand", new pauseDiscoveryOnDemand());
       processMap.put("getRemoteStorageDir", new getRemoteStorageDir());
+      processMap.put("startFingerprinting", new startFingerprinting());
       return processMap;
     }
 
@@ -1292,6 +1340,21 @@ public class Mydlp_ui {
         getRemoteStorageDir_result result = new getRemoteStorageDir_result();
         result.success = iface.getRemoteStorageDir(args.RSId);
         return result;
+      }
+    }
+
+    private static class startFingerprinting<I extends Iface> extends org.apache.thrift.ProcessFunction<I, startFingerprinting_args> {
+      public startFingerprinting() {
+        super("startFingerprinting");
+      }
+
+      protected startFingerprinting_args getEmptyArgsInstance() {
+        return new startFingerprinting_args();
+      }
+
+      protected org.apache.thrift.TBase getResult(I iface, startFingerprinting_args args) throws org.apache.thrift.TException {
+        iface.startFingerprinting(args.DDId);
+        return null;
       }
     }
 
@@ -10670,6 +10733,303 @@ public class Mydlp_ui {
 
     private void readObject(java.io.ObjectInputStream in) throws java.io.IOException, ClassNotFoundException {
       try {
+        read(new org.apache.thrift.protocol.TCompactProtocol(new org.apache.thrift.transport.TIOStreamTransport(in)));
+      } catch (org.apache.thrift.TException te) {
+        throw new java.io.IOException(te);
+      }
+    }
+
+  }
+
+  public static class startFingerprinting_args implements org.apache.thrift.TBase<startFingerprinting_args, startFingerprinting_args._Fields>, java.io.Serializable, Cloneable   {
+    private static final org.apache.thrift.protocol.TStruct STRUCT_DESC = new org.apache.thrift.protocol.TStruct("startFingerprinting_args");
+
+    private static final org.apache.thrift.protocol.TField DDID_FIELD_DESC = new org.apache.thrift.protocol.TField("DDId", org.apache.thrift.protocol.TType.I32, (short)1);
+
+    public int DDId; // required
+
+    /** The set of fields this struct contains, along with convenience methods for finding and manipulating them. */
+    public enum _Fields implements org.apache.thrift.TFieldIdEnum {
+      DDID((short)1, "DDId");
+
+      private static final Map<String, _Fields> byName = new HashMap<String, _Fields>();
+
+      static {
+        for (_Fields field : EnumSet.allOf(_Fields.class)) {
+          byName.put(field.getFieldName(), field);
+        }
+      }
+
+      /**
+       * Find the _Fields constant that matches fieldId, or null if its not found.
+       */
+      public static _Fields findByThriftId(int fieldId) {
+        switch(fieldId) {
+          case 1: // DDID
+            return DDID;
+          default:
+            return null;
+        }
+      }
+
+      /**
+       * Find the _Fields constant that matches fieldId, throwing an exception
+       * if it is not found.
+       */
+      public static _Fields findByThriftIdOrThrow(int fieldId) {
+        _Fields fields = findByThriftId(fieldId);
+        if (fields == null) throw new IllegalArgumentException("Field " + fieldId + " doesn't exist!");
+        return fields;
+      }
+
+      /**
+       * Find the _Fields constant that matches name, or null if its not found.
+       */
+      public static _Fields findByName(String name) {
+        return byName.get(name);
+      }
+
+      private final short _thriftId;
+      private final String _fieldName;
+
+      _Fields(short thriftId, String fieldName) {
+        _thriftId = thriftId;
+        _fieldName = fieldName;
+      }
+
+      public short getThriftFieldId() {
+        return _thriftId;
+      }
+
+      public String getFieldName() {
+        return _fieldName;
+      }
+    }
+
+    // isset id assignments
+    private static final int __DDID_ISSET_ID = 0;
+    private BitSet __isset_bit_vector = new BitSet(1);
+
+    public static final Map<_Fields, org.apache.thrift.meta_data.FieldMetaData> metaDataMap;
+    static {
+      Map<_Fields, org.apache.thrift.meta_data.FieldMetaData> tmpMap = new EnumMap<_Fields, org.apache.thrift.meta_data.FieldMetaData>(_Fields.class);
+      tmpMap.put(_Fields.DDID, new org.apache.thrift.meta_data.FieldMetaData("DDId", org.apache.thrift.TFieldRequirementType.DEFAULT, 
+          new org.apache.thrift.meta_data.FieldValueMetaData(org.apache.thrift.protocol.TType.I32)));
+      metaDataMap = Collections.unmodifiableMap(tmpMap);
+      org.apache.thrift.meta_data.FieldMetaData.addStructMetaDataMap(startFingerprinting_args.class, metaDataMap);
+    }
+
+    public startFingerprinting_args() {
+    }
+
+    public startFingerprinting_args(
+      int DDId)
+    {
+      this();
+      this.DDId = DDId;
+      setDDIdIsSet(true);
+    }
+
+    /**
+     * Performs a deep copy on <i>other</i>.
+     */
+    public startFingerprinting_args(startFingerprinting_args other) {
+      __isset_bit_vector.clear();
+      __isset_bit_vector.or(other.__isset_bit_vector);
+      this.DDId = other.DDId;
+    }
+
+    public startFingerprinting_args deepCopy() {
+      return new startFingerprinting_args(this);
+    }
+
+    @Override
+    public void clear() {
+      setDDIdIsSet(false);
+      this.DDId = 0;
+    }
+
+    public int getDDId() {
+      return this.DDId;
+    }
+
+    public startFingerprinting_args setDDId(int DDId) {
+      this.DDId = DDId;
+      setDDIdIsSet(true);
+      return this;
+    }
+
+    public void unsetDDId() {
+      __isset_bit_vector.clear(__DDID_ISSET_ID);
+    }
+
+    /** Returns true if field DDId is set (has been assigned a value) and false otherwise */
+    public boolean isSetDDId() {
+      return __isset_bit_vector.get(__DDID_ISSET_ID);
+    }
+
+    public void setDDIdIsSet(boolean value) {
+      __isset_bit_vector.set(__DDID_ISSET_ID, value);
+    }
+
+    public void setFieldValue(_Fields field, Object value) {
+      switch (field) {
+      case DDID:
+        if (value == null) {
+          unsetDDId();
+        } else {
+          setDDId((Integer)value);
+        }
+        break;
+
+      }
+    }
+
+    public Object getFieldValue(_Fields field) {
+      switch (field) {
+      case DDID:
+        return Integer.valueOf(getDDId());
+
+      }
+      throw new IllegalStateException();
+    }
+
+    /** Returns true if field corresponding to fieldID is set (has been assigned a value) and false otherwise */
+    public boolean isSet(_Fields field) {
+      if (field == null) {
+        throw new IllegalArgumentException();
+      }
+
+      switch (field) {
+      case DDID:
+        return isSetDDId();
+      }
+      throw new IllegalStateException();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+      if (that == null)
+        return false;
+      if (that instanceof startFingerprinting_args)
+        return this.equals((startFingerprinting_args)that);
+      return false;
+    }
+
+    public boolean equals(startFingerprinting_args that) {
+      if (that == null)
+        return false;
+
+      boolean this_present_DDId = true;
+      boolean that_present_DDId = true;
+      if (this_present_DDId || that_present_DDId) {
+        if (!(this_present_DDId && that_present_DDId))
+          return false;
+        if (this.DDId != that.DDId)
+          return false;
+      }
+
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      return 0;
+    }
+
+    public int compareTo(startFingerprinting_args other) {
+      if (!getClass().equals(other.getClass())) {
+        return getClass().getName().compareTo(other.getClass().getName());
+      }
+
+      int lastComparison = 0;
+      startFingerprinting_args typedOther = (startFingerprinting_args)other;
+
+      lastComparison = Boolean.valueOf(isSetDDId()).compareTo(typedOther.isSetDDId());
+      if (lastComparison != 0) {
+        return lastComparison;
+      }
+      if (isSetDDId()) {
+        lastComparison = org.apache.thrift.TBaseHelper.compareTo(this.DDId, typedOther.DDId);
+        if (lastComparison != 0) {
+          return lastComparison;
+        }
+      }
+      return 0;
+    }
+
+    public _Fields fieldForId(int fieldId) {
+      return _Fields.findByThriftId(fieldId);
+    }
+
+    public void read(org.apache.thrift.protocol.TProtocol iprot) throws org.apache.thrift.TException {
+      org.apache.thrift.protocol.TField field;
+      iprot.readStructBegin();
+      while (true)
+      {
+        field = iprot.readFieldBegin();
+        if (field.type == org.apache.thrift.protocol.TType.STOP) { 
+          break;
+        }
+        switch (field.id) {
+          case 1: // DDID
+            if (field.type == org.apache.thrift.protocol.TType.I32) {
+              this.DDId = iprot.readI32();
+              setDDIdIsSet(true);
+            } else { 
+              org.apache.thrift.protocol.TProtocolUtil.skip(iprot, field.type);
+            }
+            break;
+          default:
+            org.apache.thrift.protocol.TProtocolUtil.skip(iprot, field.type);
+        }
+        iprot.readFieldEnd();
+      }
+      iprot.readStructEnd();
+
+      // check for required fields of primitive type, which can't be checked in the validate method
+      validate();
+    }
+
+    public void write(org.apache.thrift.protocol.TProtocol oprot) throws org.apache.thrift.TException {
+      validate();
+
+      oprot.writeStructBegin(STRUCT_DESC);
+      oprot.writeFieldBegin(DDID_FIELD_DESC);
+      oprot.writeI32(this.DDId);
+      oprot.writeFieldEnd();
+      oprot.writeFieldStop();
+      oprot.writeStructEnd();
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder sb = new StringBuilder("startFingerprinting_args(");
+      boolean first = true;
+
+      sb.append("DDId:");
+      sb.append(this.DDId);
+      first = false;
+      sb.append(")");
+      return sb.toString();
+    }
+
+    public void validate() throws org.apache.thrift.TException {
+      // check for required fields
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out) throws java.io.IOException {
+      try {
+        write(new org.apache.thrift.protocol.TCompactProtocol(new org.apache.thrift.transport.TIOStreamTransport(out)));
+      } catch (org.apache.thrift.TException te) {
+        throw new java.io.IOException(te);
+      }
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws java.io.IOException, ClassNotFoundException {
+      try {
+        // it doesn't seem like you should have to do this, but java serialization is wacky, and doesn't call the default constructor.
+        __isset_bit_vector = new BitSet(1);
         read(new org.apache.thrift.protocol.TCompactProtocol(new org.apache.thrift.transport.TIOStreamTransport(in)));
       } catch (org.apache.thrift.TException te) {
         throw new java.io.IOException(te);
