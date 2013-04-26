28ede8b3f854208405239979f2ae84ad78f30397
Ozgen Muzac <ozgen@mydlp.com>
2013-04-26 16:30:40 +0300

    Remote storage test connection implementation is added.

--- mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/RemoteStorageService.java
+++ mydlp-ui-remoting-blazeds/src/main/java/com/mydlp/ui/remoting/blazeds/RemoteStorageService.java
@@ -1,6 +1,7 @@
 package com.mydlp.ui.remoting.blazeds;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.security.access.annotation.Secured;
 
@@ -16,6 +17,6 @@ public interface RemoteStorageService {
 	
 	public void startRemoteFingerprint(int id);
 	
-	public String testConnection(RemoteStorage rs);
+	public String testConnection(RemoteStorage remoteStorage);
 
 }
